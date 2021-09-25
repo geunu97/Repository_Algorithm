e8d2884402e8689a0fd7c0cbf7a6312095acf6d2
olo90632951 <olo90632951@gmail.com>
2021-09-25 17:01:28 +0900

    세 개의 자연수 A, B, C가 주어질 때 A × B × C를 계산한 결과에 0부터 9까지 각각의 숫자가 몇 번씩 쓰였는지를 구하기

--- (Baekjoon)Step5_Q3.py
+++ (Baekjoon)Step5_Q3.py
@@ -4,6 +4,8 @@
 #   예를 들어 A = 150, B = 266, C = 427 이라면 A × B × C = 150 × 266 × 427 = 17037300 이 되고, 
 #   계산한 결과 17037300 에는 0이 3번, 1이 1번, 3이 2번, 7이 2번 쓰였다.
 
+#풀이1
+'''
 a = int(input())
 b = int(input())
 c = int(input())
@@ -36,3 +38,14 @@ for i in range(list_len):
 
 for k in range(10):
     print(number[k])
+'''
+
+#풀이2
+a = int(input())
+b = int(input())
+c = int(input())
+
+n = list(map(int,(str(a*b*c))))
+
+for i in range(10):
+    print(n.count(i))
