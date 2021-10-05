ea149e00aa56e90b6d256ae586dd47c37953695a
olo90632951 <olo90632951@gmail.com>
2021-10-05 21:53:45 +0900

    호텔 손님 수에 따라서 방 번호 배정하기

---
+++ (Baekjoon)Step8_Q5.py
@@ -0,0 +1,26 @@
+#8단계: 기본 수학 1
+#5번 문제
+
+a = int(input())
+
+for i in range(a):
+    h,w,n = map(int,input().split())
+
+    Y = n % h 
+
+    X = n // h + 1
+
+    if Y == 0:
+        Y = str(h)
+        X = X - 1
+        X = str("{:02d}".format(X))
+    else:
+        Y = str(Y)
+        X = str("{:02d}".format(X))
+   
+    print(Y+X)
+
+
+
+  
+
