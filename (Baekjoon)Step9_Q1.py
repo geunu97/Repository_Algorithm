87dc878ee1d246dfc3a72d55d9ce895a536dbe63
olo90632951 <olo90632951@gmail.com>
2021-10-08 16:51:25 +0900

    주어진 수 N개 중에서 소수가 몇 개인지 찾기

---
+++ (Baekjoon)Step9_Q1.py
@@ -0,0 +1,29 @@
+#9단계: 기본 수학 2
+#1. 주어진 수 N개 중에서 소수가 몇 개인지 찾아서 출력하는 프로그램을 작성하시오.
+
+a = int(input())
+
+list_a = list(map(int,input().split()))
+
+count = 0
+
+for i in range(a):
+    no_count = 0
+
+    if list_a[i] == 1:
+        continue
+
+    elif list_a[i] == 2:
+        count += 1
+        continue
+
+    else:
+        for j in range(2,list_a[i]):
+            if list_a[i] % j == 0:
+                no_count = 1
+                break
+
+    if no_count != 1:
+        count += 1
+
+print(count)
