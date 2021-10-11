8913d59ad2259decfed87f1852d9bcb1b1c1e1bb
olo90632951 <olo90632951@gmail.com>
2021-10-12 00:35:33 +0900

    더 빠르게 소수 찾기

--- (Baekjoon)Step9_Q4.py
+++ (Baekjoon)Step9_Q4.py
@@ -3,25 +3,27 @@
 
 m,n = map(int,input().split())
 
-for i in range(m,n+1):
-    finish = 0
+def sosu(i):
+    sqrt_num = int(i**0.5)
 
+    for j in range(3,sqrt_num+1,2):     #약수는 대칭성이 있어서 절반만 확인하면 소수인지 확인가능
+        if i % j == 0:
+            return False
+    return True
+
+for i in range(m,n+1):
     if i == 1:
         continue
-
+    elif i == 2:
+        print(i)
+        continue
+    elif i % 2 == 0:
+        continue
     else:
-        sqrt_num = int(i ** 0.5)
-
-        for j in range(2,sqrt_num+1):
-            if i % j == 0:
-                finish = 1
-                break
-
-        if finish != 1:
+        if sosu(i):
             print(i)
-
 
         # 2부터 int(제곱근+1) 까지 반복문 돌리면 됨 , 그 반복문 돌린 수가 주어진 수와 계산하면 됨
         # 주어진 수 % i == 0 이면 아님
         # 2 ~ 제곱근 까지만 계산하는게 포인트!!
-        # 에라토스테네스의 체
+        # 에라토스테네스의 체 (2의 배수 지우고, 3의 배수 지우고, 5의 배수 지우고, 첫 수는 소수 , ...)
