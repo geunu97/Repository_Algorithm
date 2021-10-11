6fb45af4dd584e1af5475754be3b52390918a9c5
olo90632951 <olo90632951@gmail.com>
2021-10-12 00:41:33 +0900

    (골드바흐 파티션, 골드바흐 수) 2보다 큰 모든 짝수는 두 소수의 합으로 나타내기

--- (Baekjoon)Step9_Q6.py
+++ (Baekjoon)Step9_Q6.py
@@ -28,3 +28,27 @@ for i in range(a):
             if ffinish != 1:
                 print(number_1,number_2)
                 break
+
+
+'''
+#에라토스테네스의 체
+import sys
+input = sys.stdin.readline
+
+sosu = [True for _ in range(10001)]
+sosu[0] = sosu[1] = False
+
+for i in range(2, 101):
+    if sosu[i]:
+        for j in range(i*2, 10001, i):
+            sosu[j] = False
+
+t = int(input())
+
+for _ in range(t):
+    num = int(input())
+    for i in range(num//2, 10001):
+        if sosu[i] and sosu[num-i]:
+            print(num-i, i)
+            break
+'''
