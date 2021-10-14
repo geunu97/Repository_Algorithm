328659bd3d121494a9dc193c74b2b7214830074c
olo90632951 <olo90632951@gmail.com>
2021-10-14 13:58:26 +0900

    (더더 빠른 에라토스테네스의 체) (골드바흐 파티션, 골드바흐 수) 2보다 큰 모든 짝수를 두 소수의 합으로 나타내기

--- (Baekjoon)Step9_Q6.py
+++ (Baekjoon)Step9_Q6.py
@@ -1,6 +1,37 @@
 #9단계: 기본 수학 2
 #6번 문제
 
+# 더더 빠른 에라토스테네스의 체 (2의 배수 지우고, 3의 배수 지우고, 5의 배수 지우고, 첫 수는 소수 , ...)
+import sys
+input = sys.stdin.readline      #더 빠르게 입력받기
+
+list_sosu = []
+for x in range(10001):  
+    list_sosu.append(True)       # 리스트 내포 표현 sosu = [True for _ in range(10001)]
+
+list_sosu[0] = False
+list_sosu[1] = False
+
+for i in range(2, 101):
+    if list_sosu[i]:
+        for j in range(i*2, 10001, i):
+            list_sosu[j] = False
+
+a = int(input())
+
+for k in range(a):
+    num = int(input())
+    for i in range(num//2, 10001):
+        if list_sosu[i] and list_sosu[num-i]:
+            print(num-i, i)
+            break
+
+
+
+'''
+import sys
+input = sys.stdin.readline
+
 a = int(input())
 
 for i in range(a):
@@ -28,27 +59,4 @@ for i in range(a):
             if ffinish != 1:
                 print(number_1,number_2)
                 break
-
-
-'''
-#에라토스테네스의 체
-import sys
-input = sys.stdin.readline
-
-sosu = [True for _ in range(10001)]
-sosu[0] = sosu[1] = False
-
-for i in range(2, 101):
-    if sosu[i]:
-        for j in range(i*2, 10001, i):
-            sosu[j] = False
-
-t = int(input())
-
-for _ in range(t):
-    num = int(input())
-    for i in range(num//2, 10001):
-        if sosu[i] and sosu[num-i]:
-            print(num-i, i)
-            break
 '''
