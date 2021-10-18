94dc8cf150c72e2e9823e1bc1e1a845d36464476
olo90632951 <olo90632951@gmail.com>
2021-10-18 17:00:40 +0900

    피보나치 수 (메모이제이션, 단순 재귀)

--- Baekjoon_Step10_Q2.py
+++ Baekjoon_Step10_Q2.py
@@ -1,6 +1,27 @@
 #10단계: 재귀
 #2. 피보나치 수 5
 
+#방법1: 메모이제이션
+'''
+dictionary = {
+    1:1,
+    2:1
+}
+
+def fibonacci(n):
+    if n in dictionary:
+        return dictionary[n]
+    else:
+        dictionary[n] = fibonacci(n-1) + fibonacci(n-2)
+        return dictionary[n]
+
+n = int(input())
+
+print(fibonacci(n))
+'''
+
+#방법2: 단순 재귀사용 
+'''
 def fibo(a):
     if a == 0:
         return 0
@@ -14,3 +35,4 @@ def fibo(a):
 a = int(input())
 
 print(fibo(a))
+'''
