2a8ac1a4b61042a76a37b4c16d745561efbbe3ba
olo90632951 <olo90632951@gmail.com>
2021-10-24 18:10:55 +0900

    (프랙탈 구조) (재귀함수) 별 찍기

--- Baekjoon_Step10_Q3.py
+++ Baekjoon_Step10_Q3.py
@@ -1,51 +1,32 @@
 #10단계: 재귀
-#3번 문제
-
-def star(num):
-    if num == 3:
-        list_b[0][:3] = 1
-        list_b[1][0] = 1
-        list_b[1][2] = 1
-        list_b[1][1] = 0
-        list_b[2][:3] = 1 
-
-
-
-num = int(input())
-
-list_a=[]
-list_b=[]
-
-for i in range(num):
-    list_a.append(0)
-
-for j in range(num):
-    list_b.append(list_a)
-
-star(num)
-
-
-
-#2차원 리스트 사용
+#3. (프랙탈) 별 찍기 
 
 #프랙탈 : 작은 구조가 전체 구조와 닮은 형태로 끝없이 되풀이되는 구조
 
+def star(num, list_a):
+    if num == 3:
+        list_a.append(["***"])    # [ ]로  리스트 씌여서 문자열, 공백 곱셈 및 계산
+        list_a.append(["* *"])
+        list_a.append(["***"])
+        return list_a
+    else:
+        b = star(num//3,list_a)*3
 
+        for i in range(len(b)):
+            if i in list(range(num//3,num-num//3)):
+                b[i] = b[i] + [" "]*(num//3) + b[i]
+            else:
+                b[i] = b[i]*3
+        return b
 
+list_a=[]
 
-'''
-n = 9일 때
+N = int(input())
 
-*********
-* ** ** *
-*********
-***   ***
-* *   * *
-***   ***
-*********
-* ** ** *
-*********
-'''
+a = star(N,list_a)
+
+for i in range(len(a)):
+    print("".join(a[i]))    # 한칸씩 (이중리스트) join함수로 연결해서 출력하기
 
 
 
