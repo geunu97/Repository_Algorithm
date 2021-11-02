b36d9acd07a1782d21bb293a2c6145d575ee6dfe
olo90632951 <olo90632951@gmail.com>
2021-11-02 14:18:59 +0900

    (정렬) 좌표 압축, 자신의 수보다 작은 수가 몇개 있는지 찾아서 출력하기

--- Baekjoon_Step12_Q10.py
+++ Baekjoon_Step12_Q10.py
@@ -15,17 +15,15 @@ list_a = sorted(list_a)    #-10 -9 2 4
 
 dictionary = { }
 k = 0
-for i in list_a:                      #리스트 X , 딕셔너리로  => 해당 키 값에 대한 value 불러오는 것 때문에 
-    dictionary[i] = k  
+for i in list_a:                      #리스트 X , 딕셔너리로 하는 이유 => 해당 키 값에 대응하는 value값을 출력해야 하기 때문에 
+    dictionary[i] = k                 
     k += 1
 
 for j in list_list:                   #반목문 리스트로 돌리기!!
     print(dictionary[j],end=" ")       
 
-'''
-list_b=[]
-for i in range(len(list_a)):
-    list_b.append([list_a[i],i])          #[-10,0] , [-9,1] , [2,2] , [4,3]
-'''
 
+#Point
+#자신의 수와 나머지 수들 모두 비교하는게 아니라 중복제거 후
+#정렬한 후 각 인덱스 위치가 그 값이 됨 (계산횟수 적게 든다)
 
